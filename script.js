4d374835efadc31662d9bc0bcef45c2ac098f78b
Souser <sebastiancamach109@gmail.com>
2022-11-18 15:57:57 -0600

    Configuracion navbar

---
+++ script.js
@@ -0,0 +1,7 @@
+const toggle = document.querySelector('.toggle')
+const links = document.querySelector('.links')
+
+toggle.addEventListener('click', () => {
+    toggle.classList.toggle('rotate')
+    links.classList.toggle('active')
+})
