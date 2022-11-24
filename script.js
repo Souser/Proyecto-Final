b58789ff6472366dbddcca4467f1e54e4a874cfd
Souser <sebastiancamach109@gmail.com>
2022-11-23 18:37:20 -0600

    Inicio-porque el de yuliana estaba muy feo

--- script.js
+++ script.js
@@ -1,7 +1,1 @@
-const toggle = document.querySelector('.toggle')
-const links = document.querySelector('.links')
 
-toggle.addEventListener('click', () => {
-    toggle.classList.toggle('rotate')
-    links.classList.toggle('active')
-})
